82245784f423dc7438e179280bbe1ad79d1bdf83
iuripereiracastro <iuripereiracastro@enta.pt>
2023-09-22 10:39:37 +0000

    removed unnecessary code

--- ex3-range+length/ex3-live.py
+++ ex3-range+length/ex3-live.py
@@ -34,15 +34,12 @@ print('------')
 
 #5. Imprimir os elementos da lista e dizer se é par ou impar
 parCounter = 0
-houseCounter = 0
 for num in range(len(list)):
     if list[num] % 2 == 0:
-        print(f'O numero {list[num]} na casa {houseCounter} é par')
+        print(f'O numero {list[num]} na casa {num} é par')
         parCounter += 1
     else:
-        print(f'O numero {list[num]} na casa {houseCounter} é impar')
-
-    houseCounter += 1
+        print(f'O numero {list[num]} na casa {num} é impar')
 
 print(f'Numeros par: {parCounter}')
 print(f'Numeros impar: {len(list) - parCounter}')
