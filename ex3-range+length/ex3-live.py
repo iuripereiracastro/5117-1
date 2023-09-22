a790c3925195842532dbbbab013ef121e700190f
iuripereiracastro <iuripereiracastro@enta.pt>
2023-09-22 09:58:01 +0000

    exercicio utilizando range e length

---
+++ ex3-range+length/ex3-live.py
@@ -0,0 +1,31 @@
+"""
+Range e Length
+"""
+
+list = [60, 20, 40, 39, 10, 25, 40, 239, 10, 59, 28, 29, 200]
+
+#1. Imprime o numero de casas na lista
+print(len(list))
+print('------')
+
+#2. Imprime a sequencia de numeros entre 0 e o numero de casas na list - 1. Usa a variavel x
+i = 0
+for x in range(len(list)):
+    print(f'{i} = {list[x]}')
+    i += 1
+
+print('------')
+
+#3. Imprime a sequencia de numeros entre numero de casas - 1 e 0. Usa variavel y
+numero = len(list) - 1
+for y in range(len(list)):
+    print(numero)
+    numero -= 1
+
+print('------')
+
+#4. Imprime a lista na ordem inversa
+n = len(list) - 1
+for z in range(len(list)):
+    print(f'{n} = {list[n]}')
+    n -= 1
