51810828bbbc77f0d6e28321b81e86d3ded9deab
iuripereiracastro <iuripereiracastro@enta.pt>
2023-09-26 21:16:17 +0000

    ex1 calcula uma operacao

---
+++ cap2/ex1.py
@@ -0,0 +1,5 @@
+print(f'Vou pedir-lhe dois numeros')
+x = int(input(f'Escreva o primeiro numero, x = '))
+y = int(input(f'Escreva o segundo numero, y = '))
+
+print(f'O valor de ({x} + 3 * {y}) * ({x} - {y}) e: {(x + 3 * y) * (x - y)}')
