6715761e78e6b3e7007402bf327db2f3f26b82f1
iuripereiracastro <iuripereiracastro@enta.pt>
2023-09-26 22:43:50 +0000

    ex5 medium and standard deviation

--- cap2/ex5.py
+++ cap2/ex5.py
@@ -1,4 +1,24 @@
+"""
+O primeiro soma x1+x2+x3+x4+x5 (sendo que cada x é um dos 5 números que o user meteu)
+O segundo é (x1-x̄)²+(x2-x̄)²+(x3-x̄)²...
+"""
 from math import sqrt
 
-x = [1, 2, 3, 4, 5]
+x = []
+i = 0
 
+while i < 5:
+    x.append(int(input('Insira um numero\n')))
+    i += 1
+
+media = 0
+for n in x:
+    media += n
+media = media / len(x)
+
+desvio = 0
+for n in x:
+    desvio += (n - media) ** 2
+desvio = sqrt((1/4)*desvio)
+
+print(f'Media: {media}\nDesvio padrao: {desvio}')
